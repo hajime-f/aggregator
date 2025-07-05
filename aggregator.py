3d04b050536838c8d52576031cc08766c3d3b51f
Hajime Fujita <hajime.fujita@gmail.com>
2025-07-06 08:31:38 +0900

    サイトを追加

--- aggregator.py
+++ aggregator.py
@@ -1,22 +1,14 @@
 import html
 import json
 import os
-import re
 import time
 import urllib.request
 from datetime import datetime
 
 import google.generativeai as genai
-import requests
 import yaml
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Aggregator:
@@ -60,43 +52,41 @@ class Aggregator:
             except AttributeError:
                 pub_date = item.find("dc:date").get_text()
 
-            try:
-                dt_object = datetime.strptime(pub_date, "%a, %d %b %Y %H:%M:%S %Z")
-            except ValueError:
-                try:
-                    dt_object = datetime.strptime(
-                        pub_date, "%a, %d %b %Y %H:%M:%S +0000"
-                    )
-                except ValueError:
-                    dt_object = datetime.fromisoformat(pub_date)
-            is_today = dt_object.date() == datetime.now().date()
+            if key in ("Yahoo"):
+                content = self.fetch_content_yahoo(link)
+            elif key in ("橘玲", "トーマス", "Books"):
+                html_inside = item.find("content:encoded").text
+                inner_soup = BeautifulSoup(html.unescape(html_inside), "html.parser")
+                plain_text = inner_soup.get_text(separator="\n", strip=True)
+                content = plain_text.replace("\n", "").replace("\u3000", " ")
+            elif key in ("朝日新聞"):
+                content = self.fetch_content_asahi(link)
+            elif key in ("読売新聞"):
+                content = self.fetch_content_yomiuri(link)
+            elif key in ("みんかぶ"):
+                content = self.fetch_content_minkabu(link)
+            elif key in ("WIRED"):
+                content = self.fetch_content_wired(link)
 
-            if is_today:
-                if key in ("Yahoo"):
-                    content = self.fetch_content_yahoo(link)
-                elif key in ("橘玲", "トーマス", "Books"):
-                    html_inside = item.find("content:encoded").text
-                    inner_soup = BeautifulSoup(
-                        html.unescape(html_inside), "html.parser"
-                    )
-                    plain_text = inner_soup.get_text(separator="\n", strip=True)
-                    content = plain_text.replace("\n", "").replace("\u3000", " ")
-                elif key in ("朝日新聞"):
-                    content = self.fetch_content_asahi(link)
-                elif key in ("読売新聞"):
-                    content = self.fetch_content_yomiuri(link)
-                elif key in ("みんかぶ"):
-                    content = self.fetch_content_minkabu(link)
-
-                news_item = {
-                    "title": title.replace("\u3000", " "),
-                    "link": link,
-                    "pubDate": dt_object.strftime("%Y-%m-%d %H:%M"),
-                    "content": content,
-                }
-                dict_content[key].append(news_item)
+            news_item = {
+                "title": title.replace("\u3000", " "),
+                "link": link,
+                "pubDate": pub_date,
+                "content": content,
+            }
+            dict_content[key].append(news_item)
 
         return dict_content
+
+    def fetch_content_wired(self, link):
+        content = self.request_content(link)
+        soup = BeautifulSoup(content, "html.parser")
+        content_list = soup.find_all("div", class_="body__inner-container")
+        full_text = ""
+        for content in content_list:
+            full_text += content.get_text()
+
+        return full_text
 
     def fetch_content_minkabu(self, link):
         content = self.request_content(link)
@@ -117,9 +107,13 @@ class Aggregator:
     def fetch_content_asahi(self, link):
         content = self.request_content(link)
         soup = BeautifulSoup(content, "html.parser")
-        p_tags = soup.find("div", class_="nfyQp").find_all("p")
-        text_list = [p.get_text(strip=True) for p in p_tags]
-        full_text = "".join(text_list)
+
+        try:
+            p_tags = soup.find("div", class_="nfyQp").find_all("p")
+            text_list = [p.get_text(strip=True) for p in p_tags]
+            full_text = "".join(text_list)
+        except AttributeError:
+            full_text = ""
 
         return full_text
 
@@ -151,7 +145,7 @@ if __name__ == "__main__":
     prompt = f"""
     40代の男性（高学歴・高年収・コンピュータが専門・テクノロジーに強い・兵庫県西宮市在住・音楽（特に吹奏楽）が好き・資産運用に興味がある）が日々キャッチアップしておくべき情報を、次の巨大なニュースコーパスから抜き出したいと考えています。
     この目的を踏まえて、下記(1)〜(5)のアクションを実行してください。
-    (1) この男性が読んでおくべきと考えられるコンテンツを抽出する。読んでおく必要がないと判断したコンテンツは捨てて構わない。
+    (1) この男性が読んでおくべきと考えられるコンテンツを抽出する。読んでおく必要がないと判断したコンテンツは捨てて構わない。また、日付が古い（目安として２日以上前）コンテンツも捨てて構わない。
     (2) 抽出したコンテンツを横断的に分析し、類似のコンテンツを統合した上で要約する。ただし、統合・要約した元のコンテンツが特定できるように、タイトルとURLを対応づけておく。
     (3) 30個以下のトピックに対して上記要約を準備する。ただし、これらのトピックは互いに重複がないようにする。
     (4) 準備した要約をEmacsのorg-modeで表示しやすい形式にフォーマットする。ただし、適宜改行を入れるなどの処理を行って、男性が読みやすいように工夫すること。
